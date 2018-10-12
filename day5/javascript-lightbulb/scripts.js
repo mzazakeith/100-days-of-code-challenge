94f2763ac525b707a3f1474629754b65250449c4
mzazakeith <mzazakeith@gmail.com>
2018-10-12 14:26:29 +0800

    added hover function for second light bulb

--- day5/javascript-lightbulb/scripts.js
+++ day5/javascript-lightbulb/scripts.js
@@ -1,8 +1,17 @@
+const image = document.getElementById('myImage');
+const imge = document.getElementById('bulb')
 function changeImage() {
-    var image = document.getElementById('myImage');
     if (image.src.match("bulbon")) {
         image.src = "pic_bulboff.gif";
     } else {
         image.src = "pic_bulbon.gif";
+    }
+}
+
+function changeState() {
+    if (imge.src.match("bulboff")) {
+        imge.src = "pic_bulbon.gif";
+    } else{
+        imge.src = "pic_bulboff.gif"
     }
 }
