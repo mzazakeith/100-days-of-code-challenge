48add39e3e2973484d30836f19c72fe8b48ca533
mzazakeith <mzazakeith@gmail.com>
2018-10-14 19:30:02 +0800

    javascript digital clock html and js done

---
+++ day6/javascriptdigitalclock/scripts.js
@@ -0,0 +1,18 @@
+function startTime(){
+    const today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+
+}
+
+function checkTime(i) {
+    if (i<10){
+        i = "0" + i };
+    return i;
+}
+    
