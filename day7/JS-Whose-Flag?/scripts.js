cb297fa7acf52b63a9dc9bc07b9f8a6fda8313e0
mzazakeith <mzazakeith@gmail.com>
2018-10-14 20:13:16 +0800

    JS flag identifier JS done

---
+++ day7/JS-Whose-Flag?/scripts.js
@@ -0,0 +1,10 @@
+function kenya(){
+    // console.log("Kenya!!!!!")
+    document.getElementById('text').innerHTML = ("Kenya");
+}
+function malaysia(){
+    document.getElementById('text').innerHTML =("Malaysia");
+}
+function reset(){
+    document.getElementById('text').innerHTML=""
+}
